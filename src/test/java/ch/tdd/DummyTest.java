8a3744c4296496a2a3cc044ac18f9eda1f06e3f6
Marius Reusch <marius.reusch@googlemail.com>
2017-04-19 19:53:18 +0200

    Initial commit

---
+++ src/test/java/ch/tdd/DummyTest.java
@@ -0,0 +1,17 @@
+package ch.tdd;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() throws Exception {
+        assertTrue(true);
+    }
+}
